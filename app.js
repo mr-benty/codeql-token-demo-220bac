11b18ef1146a14b1cca182ba9e1319da2407d71a
neve-larsson <oxionash+neve@proton.me>
2026-08-30 05:50:13 +0200

    seed

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+function handler(req, res) {
+  const u = new URL(req.url, 'http://localhost');
+  const expr = u.searchParams.get('expr');
+  const result = eval(expr);
+  res.end('result ' + result);
+}
+http.createServer(handler);
+module.exports = { handler };
